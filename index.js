ae33ee9c099cac3a3a32a65641b53c453d0c0315
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2020-11-22 11:17:54 -0700

    added inquirer dependencies

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const Manager = require('./lib/Manager')
 // Generate the template
 const generatePage = require('./src/page-template')
 
+const array = [];
+
+const path = require("path");
+
+const output = path.resolve(__dirname, "output")
+const outputPath = path.join(output, "team.html")
+
+promptManager()
 // prompt user for manager questions function
 // array of questions to the manager that ends with a menu
 const promptManager = () => {
@@ -44,10 +52,16 @@ const promptManager = () => {
     },
     {
         type: 'input',
-        name: 'office number',
+        name: 'officeNumber',
         message: 'Please enter your office phone number.'
     },
-]);
+])
+.then(answers => {
+    const manager = new Manager(answers.name, answers.ID, answers.email, answers.officeNumber)
+    array.push(manager)
+
+    promptEmployeeInfo()
+})
 };
 
 const promptEmployeeInfo = () => {
@@ -56,53 +70,98 @@ const promptEmployeeInfo = () => {
             type: 'list',
             name: 'employees',
             message: 'Would you like to add an employee to your team? If yes, select which employee you would like to add.',
-            choices: [ engineer, intern, employee ]
+            choices: [ "engineer", "intern", "I don't want to add any more employees" ]
         }
     ])
+    .then(choice => {
+        switch (choice.employees) {
+            case "engineer" : 
+            promptEngineer()
+            break;
+
+            case "intern" :
+                promptIntern()
+                break;
+
+            default: 
+            buildTeam()
+        }
+    })
 }
 
 
 const promptEngineer = () => {
+    inquirer.prompt ([
 
-    console.log(`
-    
-    =================
-    Add Employee Info
-    =================
-    `),
     {
-        if (engineer) {
-            type: 'input',
-            name: 'name',
-            message: 'Please enter the GitHub Username for your engineer.'
-        }
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your engineer name'
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'Please enter your engineer employee ID number.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.'
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your engineer GitHub URL.'
+    },
+])
+.then(answers => {
+    const engineer = new Engineer(answers.name, answers.ID, answers.email, answers.github)
+    array.push(engineer)
+
+    promptEmployeeInfo()
+})
+};
+
+const promptIntern = () => {
+    inquirer.prompt ([
+
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your intern name'
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'Please enter your intern employee ID number.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your intern email address.'
+
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter your intern school name.'
+    },
+])
+.then(answers => {
+    const intern = new Intern(answers.name, answers.ID, answers.email, answers.school)
+    array.push(intern)
+
+    promptEmployeeInfo()
+})
+};
+
+function buildTeam() {
+    if (!fs.existsSync(output)) {
+        fs.mkdirSync(output)
     }
-    
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Please provide the name of your employee'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Please provide the employee ID number for your employee.'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Please provide the email address for your employee.'
-        },
-        {
-            if {
-
-            }
-        }
-
-    ])
+    fs.writeFilySync(outputPath, generatePage(array), "utf-8")
 }
-
 
 
 // call promptManager method
